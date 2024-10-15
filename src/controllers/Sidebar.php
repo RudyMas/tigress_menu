1831fca25f6fa694da7d91dc0e5b343dde791707
RudyMasGoNext <rudy.mas@go-next.be>
2024-10-15 15:44:00 +0200

    Made so we can use multiple json files as needed

--- src/controllers/Sidebar.php
+++ src/controllers/Sidebar.php
@@ -7,7 +7,7 @@ namespace Controller;
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2024 Rudy Mas (https://rudymas.be)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 1.1.0
+ * @version 1.1.1
  * @lastmodified 2024-10-15
  * @package Tigress\Menu
  */
@@ -16,10 +16,11 @@ class Sidebar extends Menu
     /**
      * Create the sidebar
      *
+     * @param string $jsonFile
      * @return void
      */
-    public function createSidebar(): void
+    public function createSidebar(string $jsonFile = 'config/sidebar.json'): void
     {
-        $this->menu = json_decode(file_get_contents('config/sidebar.json'));
+        $this->menu = json_decode(file_get_contents($jsonFile));
     }
 }
