4d8dbeaa1489f27c3d95b0b9725c851b42be88d0
RudyMasGoNext <rudy.mas@go-next.be>
2024-10-15 14:40:22 +0200

    header and footer css added

--- src/controllers/Menu.php
+++ src/controllers/Menu.php
@@ -9,7 +9,7 @@ use Twig\Error\LoaderError;
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2024 Rudy Mas (https://rudymas.be)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 0.1.2
+ * @version 0.1.3
  * @lastmodified 2024-10-15
  * @package Tigress\Menu
  */
@@ -37,7 +37,7 @@ class Menu
      */
     public static function version(): string
     {
-        return '0.1.2';
+        return '0.1.3';
     }
 
     /**
