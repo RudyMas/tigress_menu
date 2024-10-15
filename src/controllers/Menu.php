b89ae594feb69c011efc46032a6cd421ace5c13e
RudyMasGoNext <rudy.mas@go-next.be>
2024-10-15 13:17:55 +0200

    Added CSS files with grid-layout

--- src/controllers/Menu.php
+++ src/controllers/Menu.php
@@ -9,7 +9,7 @@ use Twig\Error\LoaderError;
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2024 Rudy Mas (https://rudymas.be)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 0.1.0
+ * @version 0.1.1
  * @lastmodified 2024-10-15
  * @package Tigress\Menu
  */
@@ -37,7 +37,7 @@ class Menu
      */
     public static function version(): string
     {
-        return '0.1.0';
+        return '0.1.1';
     }
 
     /**
