6b00d4bd00c55805f618c472cb14201656c8db97
RudyMasGoNext <rudy.mas@go-next.be>
2025-01-31 09:35:27 +0100

    Updated for dark/light setting

--- src/controllers/Menu.php
+++ src/controllers/Menu.php
@@ -10,7 +10,7 @@ use Twig\Error\LoaderError;
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2024 Rudy Mas (https://rudymas.be)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 2024.11.28.0
+ * @version 2024.11.28.1
  * @package Tigress\Menu
  */
 class Menu
