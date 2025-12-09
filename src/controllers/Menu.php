0bb6cc14b4eb5009e658a5c5c1d3cb5a646a6570
RudyMasGoNext <rudy.mas@go-next.be>
2025-12-09 14:50:56 +0100

    Update to PHP 8.5

--- src/controllers/Menu.php
+++ src/controllers/Menu.php
@@ -5,12 +5,12 @@ namespace Controller;
 use Twig\Error\LoaderError;
 
 /**
- * Class Menu (PHP version 8.4)
+ * Class Menu (PHP version 8.5)
  *
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2024-2025 Rudy Mas (https://rudymas.be)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 2025.09.22.0
+ * @version 2025.12.09.0
  * @package Tigress\Menu
  */
 class Menu
@@ -40,7 +40,7 @@ class Menu
      */
     public static function version(): string
     {
-        return '2025.09.22';
+        return '2025.12.09';
     }
 
     /**
