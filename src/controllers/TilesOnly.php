2db7d6f6c426f7da5910b487fe3092077c0abd01
RudyMasGoNext <rudy.mas@go-next.be>
2026-02-24 14:48:38 +0100

    TilesOnly also updated so info can be used

--- src/controllers/TilesOnly.php
+++ src/controllers/TilesOnly.php
@@ -3,12 +3,12 @@
 namespace Controller;
 
 /**
- * Class Tiles (PHP version 8.4)
+ * Class Tiles (PHP version 8.5)
  *
  * @author Rudy Mas <rudy.mas@rudymas.be>
- * @copyright 2025 Rudy Mas (https://rudymas.be)
+ * @copyright 2025-2026 Rudy Mas (https://rudymas.be)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 2025.09.22.0
+ * @version 2026.02.24.0
  * @package Tigress\Menu
  */
 class TilesOnly extends Menu
@@ -18,9 +18,10 @@ class TilesOnly extends Menu
      *
      * @param string|array $jsonFileOrArray
      * @param bool $withSidebar
+     * @param bool $showInfo
      * @return string
      */
-    public function createTiles(string|array $jsonFileOrArray, bool $withSidebar = false): string
+    public function createTiles(string|array $jsonFileOrArray, bool $withSidebar = false, bool $showInfo = false): string
     {
         if (file_exists(SYSTEM_ROOT . '/src/menus/' . $jsonFileOrArray)) {
             $this->menu = json_decode(file_get_contents(SYSTEM_ROOT . '/src/menus/' . $jsonFileOrArray), true);
@@ -29,9 +30,9 @@ class TilesOnly extends Menu
         }
 
         if ($withSidebar === true) {
-            $output = $this->buildTilesWithSidebar();
+            $output = $this->buildTilesWithSidebar($showInfo);
         } else {
-            $output = $this->buildTiles();
+            $output = $this->buildTiles($showInfo);
         }
 
         return $output;
@@ -50,18 +51,19 @@ class TilesOnly extends Menu
     /**
      * Build the tiles
      *
+     * @param bool $showInfo
      * @return string
      */
-    private function buildTiles(): string
+    private function buildTiles(bool $showInfo = false): string
     {
         $output = '<div class="only-tiles">';
         $output .= "<div class='row'>";
         $output .= "<div class='col-sm-12'>";
         foreach ($this->menu['tiles'] as $key => $value) {
             if (RIGHTS->checkRightsForSpecificPath($value['url'])) {
-                $output .= $this->createTile($key, $value);
+                $output .= $this->createTile($key, $value, $showInfo);
             } else {
-                $output .= $this->createGreyTile($key, $value);
+                $output .= $this->createGreyTile($key, $value, $showInfo);
             }
         }
         $output .= "</div>";
@@ -76,12 +78,17 @@ class TilesOnly extends Menu
      *
      * @param string $key
      * @param array $value
+     * @param bool $showInfo
      * @return string
      */
-    private function createTile(string $key, array $value): string
+    private function createTile(string $key, array $value, bool $showInfo = false): string
     {
         $output = "<a href='{$value['url']}' target='{$value['target']}' style='overflow: hidden'>";
-        $output .= "<div class='{$value['button']} {$value['buttonColorClass']}'>";
+        if ($showInfo === true && !empty($value['info'])) {
+            $output .= "<div class='{$value['button']} {$value['buttonColorClass']}' title='{$value['info']}' data-toggle='tooltip'>";
+        } else {
+            $output .= "<div class='{$value['button']} {$value['buttonColorClass']}'>";
+        }
         $output .= "<i class='{$value['icon']} fa-2x' style='color: {$value['iconColor']}'></i>";
         $output .= "<span class='label'>{$key}</span>";
         $output .= '</div>';
